20aed163b0c959292f96acb9b734fe5dde989f35
JP Verkamp <me@jverkamp.com>
2021-04-11 19:44:01 -0400

    Don't reload on favorite

--- static/script.js
+++ static/script.js
@@ -5,5 +5,7 @@ function favorite(el, date, hash) {
         body: JSON.stringify({'date': date, 'hash': hash, 'text': el.parentNode.children[0].innerText})
     })
 
-    el.setAttribute('data-favorite', el.attributes["data-favorite"].value === "True" ? "False" : "True");
+    el.setAttribute('data-favorite', el.attributes["data-favorite"].value === "True" ? "False" : "True")
+
+    return false
 }
